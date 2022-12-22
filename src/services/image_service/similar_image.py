19c339835146c74c74192da09fc1242a4d4921cb
beckachuu <minhtrang072121@gmail.com>
2022-12-22 08:47:47 +0700

    fix log out fe

--- src/services/image_service/similar_image.py
+++ src/services/image_service/similar_image.py
@@ -1,9 +1,10 @@
 from operator import itemgetter
-
+import urllib.request
 import numpy as np
 from tensorflow.keras.applications.vgg16 import VGG16, preprocess_input
 from tensorflow.keras.models import Model
 from tensorflow.keras.preprocessing.image import img_to_array, load_img
+import os
 
 
 class FeatureExtractor:
@@ -15,10 +16,16 @@ class FeatureExtractor:
         #                    outputs=base_model.get_layer('fc1').output)
         pass
 
-    def extract(self, img):
-        x = np.expand_dims(img, axis=0)
-        x = preprocess_input(x)
-        feature = self.model.predict(x)[0]
+    def extract(self, image_url):
+        img = load_img(image_url)
+        img = img.resize((224, 224))  # VGG must take a 224x224 img as an input
+        img = img.convert('RGB')  # Make sure img is color
+        # To np.array. Height x Width x Channel. dtype=float32
+        x = image.img_to_array(img)
+        # (H, W, C)->(1, H, W, C), where the first elem is the number of img
+        x = np.expand_dims(x, axis=0)
+        x = preprocess_input(x)  # Subtracting avg values for each pixel
+        feature = self.model.predict(x)[0]  # (1, 4096) -> (4096, )
         return feature / np.linalg.norm(feature)
 
 
@@ -36,12 +43,12 @@ def get_most_similar(books, image_url, max_result_num):
 
     books_and_scores = []
     for book in books:
-        img = load_img(image_url, target_size=(224, 224))
-        img = img_to_array(img) / 255.0
-        cover = load_img(book.cover, target_size=(224, 224))
-        cover = img_to_array(cover) / 255.0
-        cover_pred = Extractor.extract(img)
+        if not book.preds:
+            cover_url = 'src/services/image_service/'+book.book_id+'.png'
+            if not os.path.isfile():
+                urllib.request.urlretrieve(book.cover, cover_url)
 
+        cover_pred = Extractor.extract(img)
         score = eucledian_distance(pred, cover_pred)
         books_and_scores.append((book, score))
 
